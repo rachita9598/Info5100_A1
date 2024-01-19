a424a064f6a9edb55e99cb22bc30dd4db065019b
unknown <rachitas9598@gmail.com>
2024-01-19 14:20:27 -0500

    Added screenshot successfully!

---
+++ Hello_World_rachita/src/hello_world_rachita/Hello_World_rachita.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hello_world_rachita;
+
+/**
+ *
+ * @author HP
+ */
+public class Hello_World_rachita {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here\
+        System.out.println("Hello World");
+    }
+    
+}
